db84d9c2750df3555d055219d8995a907539abe3
Victor Rutskin <Lashaka250375@gmail.com>
2023-01-29 17:00:57 +0200

    getting location and camera

--- DataGathering.js
+++ DataGathering.js
@@ -1,3 +1,30 @@
+ navigator.getUserMedia (
+  // constraints
+  {
+     video: true,
+     audio: true
+  },
+
+  // successCallback
+  function(localMediaStream) {
+     var video = document.querySelector('video');
+     video.src = window.URL.createObjectURL(localMediaStream);
+     video.onloadedmetadata = function(e) {
+        // Do something with the video here.
+     };
+  },
+
+  // errorCallback
+  function(err) {
+   if(err === PERMISSION_DENIED) {
+     // Explain why you need permission and how to update the permission setting
+   }
+  }
+);
+
+//waiting for user response before data gathering
+
+
 // Set Dataset
 let DataOnUser =[];
 
@@ -13,17 +40,18 @@ fetch('http://ip-api.com/json')
 let language = navigator.language;
 DataOnUser.push("Language: " + language);
 
-// Get browser of the person visiting the website
-let browser = navigator.userAgent;
-DataOnUser.push("Browser: " + browser);
-
 // Get operating system of the person visiting the website 
 let os = navigator.platform;
 DataOnUser.push("Operating System: " + os);
 
 // Get referrer of the person visiting the website // EMPTY DATA = WORKS OR?? (NEED TO TEST)
 let referrer = document.referrer;
-DataOnUser.push("Reffering Website: " + referrer);
+if(referrer=="")
+{
+  referrer= "No Refferer";
+}
+ DataOnUser.push("Reffering Website: " + referrer);
+
 
 // Get time of visit 
 let time = new Date();
@@ -77,3 +105,47 @@ if (navigator.geolocation) {
 } else {
   console.log("Geolocation is not supported by this browser.");
 }
+
+// Get User Agent String (browser, system etc)
+let userAgent = navigator.userAgent;
+DataOnUser.push("User Agent: " + userAgent);
+
+// Get device type (mobile/desktop) 
+let deviceType = /mobile/i.test(userAgent) ? "Mobile" : "Desktop";
+DataOnUser.push("Device Type: " + deviceType);
+
+// Get battery level
+if (navigator.getBattery) {
+  navigator.getBattery().then(function(battery) {
+  let batteryLevel = battery.level * 100 + '%';
+  DataOnUser.push("Battery Level: " + batteryLevel);
+  });
+  } else if (navigator.battery) {
+  let batteryLevel = navigator.battery.level * 100 + '%';
+  DataOnUser.push("Battery Level: " + batteryLevel);
+  }
+
+// Get preferred language list
+let languageList = navigator.languages;
+DataOnUser.push("Preferred Language List: " + languageList);
+
+// Get device memory information
+let memory = window.performance.memory;
+DataOnUser.push("Device Memory: " + JSON.stringify(memory));
+
+// Get timezone
+let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+DataOnUser.push("Timezone: " + timezone);
+
+// Get media device information
+navigator.mediaDevices.enumerateDevices().then(function(devices) {
+  devices.forEach(function(device) {
+  DataOnUser.push("Media Device: " + device.kind + " - " + device.label + " - " + device.deviceId);
+  });
+  });
+  
+  // Get online/offline status (if connected to the network)
+  let onlineStatus = window.navigator.onLine ? 'Online' : 'Offline';
+  DataOnUser.push("Online Status: " + onlineStatus);
+
+//let data = navigator.
